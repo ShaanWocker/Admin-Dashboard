0d235fd72ad11553a08ec5d17f563df58bbe708f
ShaanWocker <ShaanWocker@gmail.com>
2021-12-08 11:06:25 +0200

    Login

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <input  type="input" placeholder="username" />
+            <input  type="password" placeholder="password" />
+            <button>Login</button>
+        </div>
+    );
+};
+
+export default Login;
