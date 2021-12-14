4e13f5aae3ff8e46934b7d99be54b62654587ca4
ShaanWocker <ShaanWocker@gmail.com>
2021-12-14 23:02:25 +0200

    basic setup

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,5 +1,20 @@
 import { loginStart, loginFailure, loginSuccess } from "./userRedux";
 import { publicRequest } from "../requestMethods";
+import { userRequest } from "../requestMethods";
+import { 
+    getProductStart, 
+    getProductFailure, 
+    getProductSuccess, 
+    deleteProductStart,
+    deleteProductFailure,
+    deleteProductSuccess,
+    updateProductStart,
+    updateProductFailure,
+    updateProductSuccess,
+    addProductStart,
+    addProductFailure,
+    addProductSuccess,
+} from "./productRedux";
 
 export const login = async (dispatch, user) => {
     dispatch(loginStart());
@@ -8,5 +23,45 @@ export const login = async (dispatch, user) => {
         dispatch(loginSuccess(res.data));
     }catch(err){
         dispatch(loginFailure());
+    }
+};
+
+export const getProducts = async (dispatch) => {
+    dispatch(getProductStart());
+    try{
+        const res = await publicRequest.get("/products");
+        dispatch(getProductSuccess(res.data));
+    }catch(err){
+        dispatch(getProductFailure());
+    }
+};
+
+export const deleteProduct = async (id, dispatch) => {
+    dispatch(deleteProductStart());
+    try{
+        //const res = await userRequest.delete(`/products/${id}`);
+        dispatch(deleteProductSuccess(id));
+    }catch(err){
+        dispatch(deleteProductFailure());
+    }
+};
+
+export const updateProduct = async (id, product, dispatch) => {
+    dispatch(updateProductStart());
+    try{
+        //const res = await userRequest.delete(`/products/${id}`);
+        dispatch(updateProductSuccess({ id, product }));
+    }catch(err){
+        dispatch(updateProductFailure());
+    }
+};
+
+export const addProduct = async (product, dispatch) => {
+    dispatch(addProductStart());
+    try{
+        const res = await userRequest.post(`/products`, {product});
+        dispatch(addProductSuccess(res.data));
+    }catch(err){
+        dispatch(addProductFailure());
     }
 };
